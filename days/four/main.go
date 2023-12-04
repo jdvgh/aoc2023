17bd4cc83a8d8a1f3421afb1d49ab88e519b9b51
Eilers, Jonas <133217951+jdvgh@users.noreply.github.com>
2023-12-04 17:02:49 +0100

    feat: Add AOC2023-04 Part One

---
+++ days/four/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"log"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type DayfourInput struct {
+	a []string
+}
+type Card struct {
+	id               int
+	winningNumbers   []int
+	numbersScratched []int
+}
+
+var lut2 = []int{0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512}
+
+func parseNumbers(numberString string) []int {
+	numberStrings := strings.Split(strings.TrimSpace(numberString), " ")
+	var numbers []int
+	for _, numberA := range numberStrings {
+		if numberA != "" {
+			res, err := strconv.Atoi(numberA)
+			if err != nil {
+				log.Panicf("Could not parse %v as number\n", numberA)
+			} else {
+				numbers = append(numbers, res)
+			}
+		}
+
+	}
+	slices.Sort(numbers)
+	return numbers
+
+}
+
+func parseCardId(line string) int {
+	cardStringSplit := strings.Split(strings.TrimSpace(line), " ")
+	numberString := cardStringSplit[len(cardStringSplit)-1]
+	res, err := strconv.Atoi(numberString)
+	if err != nil {
+		log.Panicf("Could not parse %v as number\n", numberString)
+	}
+	return res
+
+}
+func parseCard(line string) Card {
+
+	cardSplit := strings.Split(line, "|")
+	winningNumbersSplit := strings.Split(cardSplit[0], ":")
+	cardId := parseCardId(winningNumbersSplit[0])
+	winningNumbers := parseNumbers(winningNumbersSplit[1])
+	numbersScratched := parseNumbers(cardSplit[1])
+
+	return Card{
+		id:               cardId,
+		winningNumbers:   winningNumbers,
+		numbersScratched: numbersScratched}
+
+}
+
+func countHits(card Card) int {
+	hits := 0
+	for i := 0; i < len(card.numbersScratched); i++ {
+		_, found := slices.BinarySearch(card.winningNumbers, card.numbersScratched[i])
+		if found {
+			hits = hits + 1
+		}
+	}
+
+	return hits
+
+}
+
+func countPoints(hits int) int {
+	return lut2[hits]
+}
+
+func Dayfour(in DayfourInput) int {
+	sum := 0
+
+	for _, line := range in.a {
+		card := parseCard(line)
+		hits := countHits(card)
+		points := countPoints(hits)
+		sum = sum + points
+
+	}
+
+	return sum
+}
+func main() {
+}
